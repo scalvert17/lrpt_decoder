7daff370fec06137ee7a866a6083dfbe6541e574
sammy <scalvert1523@gmail.com>
2023-12-13 15:17:08 -0500

    added mig and fifos, conv. deinterleaver not building on board tests

--- hdl/conv_deinterleaver.sv
+++ hdl/conv_deinterleaver.sv
@@ -1,7 +1,6 @@
 `timescale 1ns / 1ps
 `default_nettype none
 
-// TODO: Remove the top 8 bits
 module conv_deinterleaver #(
   parameter NUM_INP = 128, // Input is 128, 8 bit values
   parameter INPUT_SIZE = 8, // Each reading is 8 bit vaule
@@ -15,7 +14,7 @@ module conv_deinterleaver #(
   input vaild_in,
   input first_data,
   input last_data,
-  input ready_out_s,
+  input ready_out_last,
 
   output ready_in,
   output valid_out,
@@ -119,74 +118,61 @@ module conv_deinterleaver #(
     .sys_rst(!sys_rst_200) // active low
   );
 
+  logic inp_s_valid;
+  logic inp_s_ready;
+  logic signed [7:0] inp_data_s;
 
-  logic clk_8125;
-  logic clk_out;
-  logic locked;
+  logic inp_m_valid;
+  logic inp_m_ready;
+  logic signed [7:0] inp_data_m;
+  logic [31:0] inp_read_data_count;
 
-  logic [7:0] fifo_soft_inp_i;
-  logic wr_en_inp;
-  logic full_inp;
-  logic a_full_inp;
-
-  logic [7:0] fifo_soft_inp_o;
-  logic r_en_inp;
-  logic empty_inp;
-   
-  m_axis_data_fifo_v2_0_10_top #(
-    .C_FIFO_DEPTH(2048),
-    .C_IS_ACLK_ASYNC(1)
-  ) inp_fifo (
-    .rst(sys_rst),
-    .wr_clk(clk),
-    .din(fifo_soft_inp_i),
-    .wr_en(wr_en_inp),
-    .full(full_inp),
-    .almost_full(a_full_inp),
-    .rd_clk(clk_8125),
-    .dout(fifo_soft_inp_0),
-    .rd_en(r_en_inp),
-    .empty(empty_inp)
+  axis_data_fifo_0 inp_fifo (
+    .s_axis_aresetn(sys_rst),          // input wire s_axis_aresetn
+    .s_axis_aclk(clk),                // input wire s_axis_aclk
+    .s_axis_tvalid(inp_s_valid),            // input wire s_axis_tvalid
+    .s_axis_tready(inp_s_ready),            // output wire s_axis_tready
+    .s_axis_tdata(inp_data_s),              // input wire [7 : 0] s_axis_tdata
+    .m_axis_aclk(ui_clk),                // input wire m_axis_aclk
+    .m_axis_tvalid(inp_m_valid),            // output wire m_axis_tvalid
+    .m_axis_tready(inp_m_ready),            // input wire m_axis_tready
+    .m_axis_tdata(inp_read_data_count)              // output wire [7 : 0] m_axis_tdata
+    /* .axis_wr_data_count(axis_wr_data_count),  // output wire [31 : 0] axis_wr_data_count */
+    /* .axis_rd_data_count(axis_rd_data_count),  // output wire [31 : 0] axis_rd_data_count */
+    /* .almost_empty(almost_empty),              // output wire almost_empty */
+    /* .prog_empty(prog_empty),                  // output wire prog_empty */
+    /* .almost_full(almost_full),                // output wire almost_full */
+    /* .prog_full(prog_full)                    // output wire prog_full */
   );
 
-  logic [7:0] fifo_soft_out_i;
-  logic wr_en_out;
-  logic full_out;
-  logic a_full_out;
+  logic out_s_valid;
+  logic out_s_ready;
+  logic signed [7:0] out_data_s;
 
-  logic [7:0] fifo_soft_out_o;
-  logic r_en_out;
-  logic empty_out;
+  logic out_m_valid;
+  logic out_m_ready;
+  logic signed [7:0] out_data_m;
 
-  m_axis_data_fifo_v2_0_10_top #(
-    .C_FIFO_DEPTH(2048),
-    .C_IS_ACLK_ASYNC(1)
-  ) out_fifo (
-    .rst(sys_rst),
-    .wr_clk(clk),
-    .din(fifo_soft_out_i),
-    .wr_en(wr_en_out),
-    .full(full_out),
-    .almost_full(a_full_out),
-    .rd_clk(clk_8125),
-    .dout(fifo_soft_out_o),
-    .rd_en(r_en_out),
-    .empty(empty_out)
+  logic [31:0] out_write_data_count;
+
+  axis_data_fifo_0 out_fifo (
+    .s_axis_aresetn(ui_clk_sync_rst),          // input wire s_axis_aresetn
+    .s_axis_aclk(ui_clk),                // input wire s_axis_aclk
+    .s_axis_tvalid(out_s_valid),            // input wire s_axis_tvalid
+    .s_axis_tready(out_s_ready),            // output wire s_axis_tready
+    .s_axis_tdata(out_data_s),              // input wire [7 : 0] s_axis_tdata
+    .m_axis_aclk(clk),                // input wire m_axis_aclk
+    .m_axis_tvalid(out_m_valid),            // output wire m_axis_tvalid
+    .m_axis_tready(out_m_ready),            // input wire m_axis_tready
+    .m_axis_tdata(out_data_m),              // output wire [7 : 0] m_axis_tdata
+    /* .axis_wr_data_count(axis_wr_data_count),  // output wire [31 : 0] axis_wr_data_count */
+    .axis_rd_data_count(out_write_data_count)  // output wire [31 : 0] axis_rd_data_count
+    /* .almost_empty(almost_empty),              // output wire almost_empty */
+    /* .prog_empty(prog_empty),                  // output wire prog_empty */
+    /* .almost_full(almost_full),                // output wire almost_full */
+    /* .prog_full(prog_full)                    // output wire prog_full */
   );
-   
 
-  /* clk_wiz_0 mig_clk */
-  /*  ( */
-  /*   // Clock out ports */
-  /*   .clk_out1(clk_out),     // output clk_out1 */
-  /*   // Status and control signals */
-  /*   .reset(sys_rst), // input reset */
-  /*   .locked(locked),       // output locked */
-  /*  // Clock in ports */
-  /*   .clk_in1(clk)      // input clk_in1 */
-  /* ); */
-
-  /* assign clk_8125 = clk_out & locked; */
   typedef enum {
     IDLE = 0,
     READ_IN = 1,
@@ -203,63 +189,191 @@ module conv_deinterleaver #(
     // Initial 
     // Then read in if both fifos are empty (Should make both fifos 36Kb)
     if (sys_rst) begin
-      wr_en_inp <= 0;
+      inp_s_valid <= 0;
       hold_inp_q <= 0;
     end else begin
       unique case (state)
         IDLE: begin
-          ready_in <= 1;
+          if (inp_s_ready && !valid_in) begin
+            ready_in <= 1;
+          end else begin
+            ready_in <= 0;
+          end
           if (valid_in) begin
-            inp_q_hold <= soft_out_1;
+            inp_q_hold <= input_soft_1;
             state <= READ_IN;
             last_data_0 <= 0;
             hold_inp_q <= 1;
             ready_in <= 0; // Need cycle to serialize inp
-            // TODO: Write the 0th value to the fifo buffer
+            inp_s_valid <= 1;
+            inp_data_s <= input_soft_0;
+          end else begin
+            inp_s_valid <= 0;
           end
         end
         READ_IN: begin
           if (hold_inp_q) begin
-            if (last_data_0) begin
-              last_data_0 <= 0;
-              state <= WAIT_OUT;
+            if (!inp_s_ready) begin
+              inp_s_valid <= 0;
               ready_in <= 0;
-            end else begin 
-              ready_in <= 1;
+            end else begin
+              if (last_data_0) begin
+                last_data_0 <= 0;
+                state <= WAIT_OUT;
+                ready_in <= 0;
+              end else begin 
+                ready_in <= 1;
+              end
+              inp_s_valid <= 1;
+              inp_data_s <= hold_inp_q;
+              hold_inp_q <= 0;
             end
-            hold_inp_q <= 0;
-            // TODO: Write the 1st value to the fifo buffer 
           end else begin
             if (valid_in) begin
               ready_in <= 0;
               hold_inp_q <= 1;
               inp_q_hold <= soft_out_1;
-              //TODO: Wrte the 0th valu to fifo
+              inp_s_valid <= 1;
+              inp_data_s <= input_soft_0;
               if (last_data) begin
                 last_data_0 <= 1;
               end
-            end 
+            end else begin
+              inp_s_valid <= 0;
+            end
           end
         end
         WAIT_OUT: begin
-          if (empty_inp && empty_out) begin
+          // Maybe go with rd_data_count
+          inp_s_valid <= 0;
+          ready_in <= 0;
+          if (inp_read_data_count == 0 && out_write_data_count == 0) begin
             state <= IDLE;
           end
         end
       endcase
     end
   end
-  
-  // Should be for output of the last fifo
-  always_ff @(posedge clk) begin
-    if (sys_rst) begin
-      valid_out <= 0;
-    end else if (ready_out_s && not empty_out) begin
-      // Gonna want to use the read_out 
-    end 
+
+  assign deint_out = out_data_m;
+  assign valid_out = out_m_valid;
+  assign out_m_ready = ready_out_last;
+
+  typedef enum {
+    IDLE_W = 0,
+    CALC_DEL = 1,
+    CALC_ADDR = 2
+  } mig_states;
+
+  // TODO: cite meteor and the MiG from andrew
+  //
+  mig_states mig_state;
+  logic [$clog2(72)-1:0] cur_branch;
+  parameter BIG_DEL = 72 * 2048;
+  logic [$clog2(BIG_DEL)-1:0] offset;
+  logic [$clog2(80*32):0] wr_counter;
+  logic sync_mark_count;
+  logic signed [7:0] mig_hold_val;
+  logic [$clog2(36*36*2048):0] delay;
+
+  // TODO: Logic for interfacing witho MiG and the fifos
+  assign app_sr_req = 0;    // We aren't using these signals.
+  assign app_ref_req = 0;
+  assign app_zq_req = 0;
+
+  always_ff @(posedge ui_clk) begin
+    if (ui_clk_sync_rst) begin
+      app_addr <= 0;
+      sync_mark_count <= 0;
+      mig_state <= IDLE_W;
+      wr_counter <= 0;
+    end else begin
+      // Assign valid_in between equal to zero if this is the case
+      case (mig_state)
+        IDLE_W: begin
+          app_en <= 0;
+          if (wr_counter == 80 * 32) begin
+            state <= IDLE_R;
+            wr_counter <= 72 * 32; // Removed the sync marker bits
+            app_addr <= (offset + 36 * 2048) % BIG_DEL;
+          end else begin
+            app_cmd <= 0;
+            app_wdf_data <= 0;
+            app_wdf_end <= 0;
+            app_wdf_wren <= 0;
+            app_wdf_mask <= 0;
+            if (inp_m_valid) begin
+              state <= CALC_DEL;
+              inp_m_ready <= 0;
+              mig_hold_val <= inp_data_m;
+            end else begin
+              inp_m_ready <= 1;
+            end
+          end
+        end
+        CALC_DEL: begin
+          delay <=  ((cur_branch == 36) ? 0 : cur_branch) * 2048 * 36;
+          // Check if sync marker and increment count
+          
+          if (app_wdf_rdy) begin
+            if (cur_branch == 0 && sync_mark_count != 7) begin
+                wr_counter <= wr_counter + 1;
+                sync_mark_count <= sync_mark_count + 1;
+                state <= IDLE_W;
+            end else begin
+              sync_mark_count <= 0;
+              app_cmd <= 0;
+              app_en <= 0;
+              app_wdf_data <= mig_hold_val;
+              app_wdf_end <= 1;
+              app_wdf_wren <= 1;
+              app_wdf_mask <= 0;
+              state <= CALC_ADDR;
+            end
+          end
+        end
+        CALC_ADDR: begin
+          if (app_rdy) begin
+            // Write to addr
+            app_addr = (offset - delay + BIG_DEL) % BIG_DEL;
+            app_cmd = 0;
+            app_en = 1;
+            app_wdf_data = 0;
+            app_wdf_end = 0;
+            app_wdf_wren = 0;
+            app_wdf_mask = 0;
+            state <= IDLE_W;
+            offset <= (offset == BIG_DEL - 1) ? 0 : offset + 1;
+            cur_branch <= (cur_branch == 72 - 1) ? 0 : cur_branch + 1;
+            wr_counter <= wr_counter + 1;
+          end
+        end
+        IDLE_R: begin
+          // Make 80 * 32 reads
+          // Should at least reduce overflow at the recivening fifo
+          //
+          if (app_rdy && out_s_ready) begin
+            app_addr <= wr_counter;
+            app_cmd <= 1;
+            app_en <= 1;
+            app_wdf_data <= 0;
+            app_wdf_end <= 0;
+            app_wdf_wren <= 0;
+            app_wdf_mask <= 0;
+            wr_counter <= wr_counter - 1;
+          end else begin 
+            app_en <= 0;
+          end
+          if (wr_counter == 0) begin
+            state <= IDLE_R;
+          end
+        end
+      endcase
+    end
   end
 
-  // Gonna want to do the other 
+  assign app_rd_data = out_data_s; 
+  assign app_rd_data_valid = out_s_valid;
   
 
 endmodule
